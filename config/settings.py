8ebba4e7cf1065e980e8f937f34799f9e9983540
DmitriyFi <fisenko.business@mail.ru>
2022-08-16 21:35:15 +0200

    updated heroku settings

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG')
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
@@ -88,7 +88,7 @@ DATABASES = {
     }
 }
 
-# Localhost DB settings:
+# # Localhost DB settings:
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql_psycopg2",
