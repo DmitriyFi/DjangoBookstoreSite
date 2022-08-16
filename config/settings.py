137208e869d9ab1e8e90b3a89d6a2509c92402f3
DmitriyFi <fisenko.business@mail.ru>
2022-08-16 18:49:07 +0200

    Heroku config

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c1nur6ve=fg$$q$$9ikj5uz9qb098jb+&re&!h6-)-$$c#$%^x2gdgs'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DJANGO_DEBUG')
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
